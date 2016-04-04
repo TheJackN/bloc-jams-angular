1be40d168814d3335f43f30172e05c1d3f044363
Jay <jamescarlson2@hotmail.com>
2016-04-04 13:14:12 -0500

    Refactor play functionality with private setSong function

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,11 +29,20 @@
       currentSong = song;
     };
 
+    /*
+    @function playSong
+    @desc plays currentBuzzObject and sets playing attribute to true
+    @param {Object} song
+    */
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    }
+
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
 
       } else if (currentSong === song) {
           if (currentBuzzObject.isPaused()) {
