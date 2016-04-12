6f86b98b5607f2c5ff3f7a545c586ffcabf1524b
Jay <jamescarlson2@hotmail.com>
2016-04-12 10:51:23 -0500

    Update seekbar to seek through playing song

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function (){
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     var SongPlayer = {};
 
     var currentAlbum = Fixtures.getAlbum();
@@ -24,6 +24,12 @@
       currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
+      });
+
+      currentBuzzObject.bind('timeupdate', function() {
+        $rootScope.$apply(function() {
+          SongPlayer.currentTime = currentBuzzObject.getTime();
+        });
       });
 
       SongPlayer.currentSong = song;
@@ -57,6 +63,12 @@
     @type {Object}
     */
     SongPlayer.currentSong = null;
+
+    /*
+    @desc Current playback time (in seconds) of currently playing song
+    @type {Number}
+    */
+    SongPlayer.currentTime = null;
 
     /*
     @function play
@@ -117,10 +129,21 @@
       }
     };
 
+    /*
+      @function setCurrentTime
+      @desc Set current time (in seconds) of currently playing song
+      @param {Number} time
+    */
+    SongPlayer.setCurrentTime = function(time) {
+      if (currentBuzzObject) {
+          currentBuzzObject.setTime(time);
+      }
+    };
+
     return SongPlayer;
   }
 
   angular
     .module('blocJams')
-    .factory('SongPlayer', SongPlayer);
+    .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
